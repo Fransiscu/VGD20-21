a2351cb44b1584ba01653153e6f64f0ca3e6f88c
Fran Soru <fran.soru@gmail.com>
2021-02-08 15:21:00 +0100

    First character animations working properly

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
-    public int playerSpeed = 10; 
-    private bool facingRight = true;
-    public int playerJumpPower = 50; // to change/powerup
     private float horizontalMovement;
+    public int playerSpeed = 10; 
+    public int playerJumpPower = 50; // to change/powerup
+    private bool facingRight = true;
     public bool touchingGround; 
+
+    private Animator animator;
 
     void Start()
     {
-        
+        touchingGround = true;
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -25,17 +28,61 @@ public class playerMovement : MonoBehaviour
     {
         horizontalMovement = Input.GetAxis("Horizontal");
 
+        if (horizontalMovement == 0)    // if the character is not moving
+        {
+            if (!touchingGround)    // and is jumping
+            {
+                animator.SetBool("isIdle", false);
+                animator.SetBool("isWalking", false);
+            }
+            else    // and is not jumping
+            {
+                animator.SetBool("isIdle", true);
+                animator.SetBool("isWalking", false);
+            }
+        } 
+        else    // if the character is moving
+        {
+            if (!animator.GetBool("isWalking")) // and if is not already walking
+            {
+                animator.SetBool("isIdle", false);
+                animator.SetBool("isWalking", true);
+            }
+
+            if (!touchingGround)
+            {
+                animator.SetBool("isWalking", false);
+            }
+        }
+
         if (Input.GetButtonDown("Jump") && touchingGround)
         {
+            animator.SetBool("isIdle", false);
+            animator.SetBool("isJumping", true);
             Jump(); 
         }
 
         if (horizontalMovement > 0.0f && facingRight == false)   // moving right
         {
+            animator.SetBool("isIdle", false);
+            animator.SetBool("isWalking", true);
+            animator.SetBool("isJumping", false);
+            if (!touchingGround)
+            {
+                animator.SetBool("isWalking", false);
+            }
             FlipPlayer(); 
         }
         else if (horizontalMovement < 0.0f && facingRight == true)   // moving left
         {
+            animator.SetBool("isIdle", false);
+            animator.SetBool("isWalking", true);
+            animator.SetBool("isJumping", false);
+            
+            if (!touchingGround)
+            {
+                animator.SetBool("isWalking", false);
+            }
             FlipPlayer(); 
         }
 
@@ -68,6 +115,7 @@ public class playerMovement : MonoBehaviour
         if (col.gameObject.tag == "Ground")
         {
             touchingGround = true;
+            animator.SetBool("isJumping", false); // back to the ground
         }
     }
 
