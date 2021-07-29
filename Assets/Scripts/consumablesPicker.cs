1ebbac4e2647592a270f91a6e25bdfffffd49be0
Fran Soru <fran.soru@gmail.com>
2021-07-29 21:34:20 +0200

    Starting with level 3

--- Assets/Scripts/consumablesPicker.cs
+++ Assets/Scripts/consumablesPicker.cs
@@ -19,43 +19,60 @@ public class consumablesPicker : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D col)
     {
         playerController = gameObject.GetComponent<playerController>();
-        
-        if (col.gameObject.tag == "Coin" && !col.GetComponent<coinController>().pickedUp)
+
+        switch (col.gameObject.tag)
         {
-            Debug.Log("Picked coin's value = " + col.GetComponent<coinController>().coinValue);
+            case "Coin" when !col.GetComponent<coinController>().pickedUp:
+                Debug.Log("Picked coin's value = " + col.GetComponent<coinController>().coinValue);
 
-            AudioSource.PlayClipAtPoint(coinPickupSound, transform.position);
+                AudioSource.PlayClipAtPoint(coinPickupSound, transform.position);
 
-            coin = col.GetComponent<coinController>();
+                coin = col.GetComponent<coinController>();
+                coinValue = coin.coinValue;
+                coin.pickedUp = true;
 
-            coinValue = coin.coinValue;
-            coin.pickedUp = true;
+                GUIController.changeGUIScore(coinValue);
+                playerController.EditScore(coinValue);
 
-            GUIController.changeGUIScore(coinValue);
-            playerController.EditScore(coinValue);
+                Destroy(col.gameObject);
+                break;
 
-            Destroy(col.gameObject);
+            case "BiggerCoin" when !col.GetComponent<doubleJumpController>().pickedUp:
+                Debug.Log("Picked bigger coin! value * 3 " + col.GetComponent<coinController>().coinValue * 3);
+
+                AudioSource.PlayClipAtPoint(coinPickupSound, transform.position);
+
+                coin = col.GetComponent<coinController>();
+                coinValue = coin.coinValue;
+                coin.pickedUp = true;
+
+                GUIController.changeGUIScore(coinValue * 3);
+                playerController.EditScore(coinValue * 3);
+
+                Destroy(col.gameObject);
+                break;
+
+            case "DoubleJump" when !col.GetComponent<doubleJumpController>().pickedUp:
+                AudioSource.PlayClipAtPoint(doubleJumpPickupSound, transform.position);
+
+                playerController.DoubleJumpEnabler();
+
+                doubleJumpController = col.GetComponent<doubleJumpController>();
+                doubleJumpController.pickedUp = true;
+
+                Destroy(col.gameObject);
+                break;
+
+            case "SpeedUp" when !col.GetComponent<speedUpController>().pickedUp:
+                AudioSource.PlayClipAtPoint(speedUpPickupSoung, transform.position);
+
+                playerController.SpeedEditEnabler(true);  // speed up -> true
+
+                speedUpController = col.GetComponent<speedUpController>();
+                speedUpController.pickedUp = true;
+
+                Destroy(col.gameObject);
+                break;
         }
-        else if (col.gameObject.tag == "DoubleJump" && !col.GetComponent<doubleJumpController>().pickedUp)
-        {
-            AudioSource.PlayClipAtPoint(doubleJumpPickupSound, transform.position);
-            playerController.DoubleJumpEnabler();
-
-            doubleJumpController = col.GetComponent<doubleJumpController>();
-            doubleJumpController.pickedUp = true;
-
-            Destroy(col.gameObject);
-        }
-        else if (col.gameObject.tag == "SpeedUp" && !col.GetComponent<speedUpController>().pickedUp) 
-        {
-            AudioSource.PlayClipAtPoint(speedUpPickupSoung, transform.position);
-            playerController.SpeedEditEnabler(true);  // speed up -> true
-
-            speedUpController = col.GetComponent<speedUpController>();
-            speedUpController.pickedUp = true;
-
-            Destroy(col.gameObject);
-        }
-
     }
 }
