29c927c0ef2750d66449924f050c2f4e3a037fe0
Fran Soru <fran.soru@gmail.com>
2021-08-24 14:15:44 +0200

    Fixed female char animations

---
+++ Assets/Scripts/GenderPickerSelectableController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class GenderPickerSelectableController : MonoBehaviour, 
+    ISelectHandler,
+    IEventSystemHandler
+{
+    private static readonly string selectedGender = DefaultValues.menuGenderSelectionPrefName;
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        PlayerPrefs.SetString(selectedGender, this.gameObject.name);
+    }
+}
