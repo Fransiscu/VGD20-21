5881f5700e259c1053d209340f02f389e285701c
Fran Soru <fran.soru@gmail.com>
2021-09-27 16:01:36 +0200

    Fixed volume and moved empress further back @ 1st level, refractoring settings

--- Assets/Scripts/Utils/SETTINGS.cs
+++ Assets/Scripts/Utils/SETTINGS.cs
@@ -4,18 +4,17 @@ using UnityEngine;
 
 public class SETTINGS
 {
-    // coins
-    public static readonly int level1MinCoinScore = 1;
-    public static readonly int level1MaxCoinScore = 10;
+    // player
+    public static readonly float startingLives = 5.0f;
+    public static readonly float basePlayerSpeed = 7f;
+    public static readonly int basePlayerJumpPower = 1450;
 
-    public static readonly int level2MinCoinScore = 2;
-    public static readonly int level2MaxCoinScore = 15;
+    // cygnus
+    public static readonly float cygnusSpeed = 325f;
+    public static readonly float cygnusDamage = 1.5f;
 
-    public static readonly int level3MinCoinScore = 5;
-    public static readonly int level3MaxCoinScore = 20;
-
-    public static readonly int bonusLevelMinCoinScore = 5;
-    public static readonly int bonusLevelMaxCoinScore = 10;
+    public static readonly float cygnusKnockBackStrength = 500.0f;
+    public static readonly float cygnusKnockBackDistance = 400.0f;
 
     // enemies
     public static readonly float level1EnemySpeed = 120f;
@@ -30,24 +29,30 @@ public class SETTINGS
     
     public static readonly float knockBackStrength = 700.0f;
     public static readonly float knockBackDistance = 450.0f;
-    
-    // cygnus
-    public static readonly float cygnusSpeed = 330f;
-    public static readonly float cygnusDamage = 1.5f;
 
-    public static readonly float cygnusKnockBackStrength = 500.0f;
-    public static readonly float cygnusKnockBackDistance = 400.0f;
-
-    // player
-    public static readonly float startingLives = 5.0f;
-    public static readonly float basePlayerSpeed = 7f;
-    public static readonly int basePlayerJumpPower = 1450;
-    
+    // status' duration
     public static readonly float invincibilityFramesDurationSeconds = 1.5f;
     public static readonly float invincibilityFramesDeltaTime = 0.15f;
 
     public static readonly float playerHitInputFreezeDuration = .5f;
     public static readonly float playerHitInputFreezeFramesDeltaTime = .1f;
+
+    // levels
+    public static readonly float soundVolume = 1f;
+    public static readonly float musicVolume = .10f;
+
+    // coins
+    public static readonly int level1MinCoinScore = 1;
+    public static readonly int level1MaxCoinScore = 10;
+
+    public static readonly int level2MinCoinScore = 2;
+    public static readonly int level2MaxCoinScore = 15;
+
+    public static readonly int level3MinCoinScore = 5;
+    public static readonly int level3MaxCoinScore = 20;
+
+    public static readonly int bonusLevelMinCoinScore = 5;
+    public static readonly int bonusLevelMaxCoinScore = 10;
 
     // powerups
     public static readonly float speedBoost = 1.3f;
@@ -56,8 +61,4 @@ public class SETTINGS
 
     // powedowns
     public static readonly float speedDeficit = .7f;
-
-    // levels
-    public static readonly float soundVolume = 1f;
-    public static readonly float musicVolume = .10f;
 }
